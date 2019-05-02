ec97427e52152af4e48fa2b42f45cd07e2c817ab
sean <sean@pavilion>
2019-05-02 17:48:46 +0100

    symfony phpunit (bridge) setup

---
+++ app/src/Compiler/FilesystemCompiler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Compiler;
+
+/**
+ * Class FilesystemCompiler
+ *
+ * This class scans all the files in a filesystem under a given path and copies/extracts any journal annotations it finds
+ *
+ * @package App\Compiler
+ */
+class FilesystemCompiler
+{
+
+}
---
+++ app/tests/Compiler/FilesystemCompilerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Compiler;
+
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class FilesystemCompilerTest extends TestCase
+{
+    public function testCompile()
+    {
+        // set up a mock filesystem crawler to return a list of paths of mixed types
+
+        // define _some_ of those paths to have annotations in them
+
+        // do the compilation
+
+        // check the journal storage for items for each annotation in each file
+    }
+}
